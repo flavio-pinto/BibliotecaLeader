8bb96845a555068aedd07e41f5153d36393b39f8
Flavio Pinto <61239151+flavio-pinto@users.noreply.github.com>
2025-02-02 19:24:00 +0100

    [CONTROLLERPRESTITI] Ottimizzazione classe e mock database dei prestiti

--- Models/Loan.cs
+++ Models/Loan.cs
@@ -3,31 +3,24 @@ namespace BibliotecaLeader.Models;
 internal class Loan
 {
     private static int _nextId = 1;
-    private string _loanId;
-    private string _bookId;
-    private string _userId;
-    private DateTime _startDate;
-    private DateTime _endDate;
-    private DateTime? _returnDate = null;
-    private decimal _penalty;
 
-    public string LoanId { get => _loanId; }
-    public string BookId { get => _bookId; set => _bookId = value; }
-    public string UserId { get => _userId; set => _userId = value; }
-    public DateTime StartDate { get => _startDate; set => _startDate = value; }
-    public DateTime EndDate { get => _endDate; set => _endDate = value; }
-    public DateTime? ReturnDate { get => _returnDate; set => _returnDate = value; }
-    public decimal Penalty { get => _penalty; set => _penalty = value; }
+    public string LoanId { get; private set; }
+    public string BookId { get; set; }
+    public string UserId { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public DateTime? ReturnDate { get; set; }
+    public decimal Penalty { get; set; }
 
     public Loan(string bookId, string userId, DateTime startDate, DateTime endDate, decimal penalty)
     {
-        _loanId = _nextId.ToString();
+        LoanId = _nextId.ToString();
         _nextId++;
-        _bookId = bookId;
-        _userId = userId;
-        _startDate = startDate;
-        _endDate = endDate;
-        _returnDate = _returnDate = null;
-        _penalty = penalty;
+        BookId = bookId;
+        UserId = userId;
+        StartDate = startDate;
+        EndDate = endDate;
+        ReturnDate = null;
+        Penalty = penalty;
     }
 }
